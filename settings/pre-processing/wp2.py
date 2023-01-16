5544ed228bd5d2efd28175eba95711e418a9d2a2
Sam J <samaneh.jozashoori@gmail.com>
2023-01-16 11:39:06 +0100

    wp2 new mapping is added!

--- settings/pre-processing/wp2.py
+++ settings/pre-processing/wp2.py
@@ -6,8 +6,8 @@ import re
 
 #################################################################################
 
-with open("/mnt/e/CLARIFY-KG-pipeline/data/wp2/gold_triples_with_sources.tsv", 'r') as tsv_file: 
-  with open("/mnt/e/CLARIFY-KG-pipeline/data/wp2/gold_triples_with_sources.csv", 'w') as csv_file:
+with open("/mnt/e/CLARIFY/wp2/batch2_processed/silver_triples.tsv", 'r') as tsv_file: 
+  with open("/mnt/e/CLARIFY/wp2/batch2_processed/silver_triples.csv", 'w') as csv_file:
     for line in tsv_file:
       new_format = re.sub("\t", ",", line)
       csv_file.write(new_format)
