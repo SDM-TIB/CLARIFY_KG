0a975e852b2e5318b72b24c7cf881a5c3d4fd4bd
Sam J <samaneh.jozashoori@gmail.com>
2023-01-17 16:16:00 +0100

    The script to change the strings in mapping is updated!

--- settings/pre-processing/MAPPING_changeStrings.py
+++ settings/pre-processing/MAPPING_changeStrings.py
@@ -5,13 +5,15 @@ import glob
 
 #################################################################################
 
-for inFile in glob.glob("/mnt/e/GitHub/CLARIFY_KG/settings/mapping/publication/*.ttl"):
+for inFile in glob.glob("/mnt/e/mapping/annotation/*.ttl"):
 	print (inFile)
 	# Read in the file
 	with open(inFile, 'r') as file :
 	  filedata = file.read()
 	# Replace the target string
-	filedata = filedata.replace("/mnt/e/data/data/publication/","/mnt/e/CLARIFY-KG-pipeline/data/publication/")
+	filedata = filedata.replace("http://clarify2020.eu/vocab/","http://research.tib.eu/clarify2020/vocab/")
+	filedata = filedata.replace("http://clarify2020.eu/entity/","http://research.tib.eu/clarify2020/entity/")
+
 	# Write the file out again
 	with open(inFile, 'w') as file:
 	  file.write(filedata)
