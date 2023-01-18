de89632fc3bda5ce9c123d3abc2fdceb66d6580e
Sam J <samaneh.jozashoori@gmail.com>
2023-01-18 16:29:03 +0100

    Update MAPPING_changeStrings.py

--- settings/pre-processing/MAPPING_changeStrings.py
+++ settings/pre-processing/MAPPING_changeStrings.py
@@ -5,14 +5,14 @@ import glob
 
 #################################################################################
 
-for inFile in glob.glob("/mnt/e/mapping/annotation/*.ttl"):
-	print (inFile)
+for inFile in glob.glob("/mnt/e/GitHub/CLARIFY-Pipeline/KGC-DIS/CSV2RDF-RMLMappingRules/wp2/*.ttl"):
 	# Read in the file
 	with open(inFile, 'r') as file :
 	  filedata = file.read()
 	# Replace the target string
-	filedata = filedata.replace("http://clarify2020.eu/vocab/","http://research.tib.eu/clarify2020/vocab/")
+	filedata = filedata.replace("/mnt/e/CLARIFY-KG-pipeline/data/wp2/","/data/wp2/")
 	filedata = filedata.replace("http://clarify2020.eu/entity/","http://research.tib.eu/clarify2020/entity/")
+	filedata = filedata.replace("http://clarify2020.eu/vocab/","http://research.tib.eu/clarify2020/vocab/")
 
 	# Write the file out again
 	with open(inFile, 'w') as file:
